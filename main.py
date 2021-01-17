e6a8e2c69d0d897ad63001dab7aa501d239e6fc6
Lana Tilke <lana.tilke@icloud.com>
2021-01-17 17:59:33 -0500

    Doesn't work yet, file dialog causing "Process finished with exit code -1073740791 (0xC0000409)".

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QVBoxLayout,
     QGroupBox,
+    QTableWidget,
+    QTableWidgetItem,
     QFileDialog,
     QFormLayout,
     QLabel,
@@ -44,25 +46,64 @@ class WavelengthWidget(QWidget):
 
 
 class DataWidget(QWidget):
+    file_list = {}
+
     def __init__(self):
         super().__init__()
-    #     outer_layout = QVBoxLayout()
-    #     self.setLayout(outer_layout)
-    #     outer_layout.addWidget(QLabel("Data Sub-Widget"))
-    #
-    #     file_button = QPushButton("Select Files")
-    #     outer_layout.addWidget(file_button)
-    #     file_button.clicked.connect(self.get_files())
-    #
-    # @staticmethod
-    # def get_files():
-    #     file_dialog = QFileDialog()
-    #     file_dialog.setFileMode(QFileDialog.AnyFile)
-    #     file_dialog.setFilter("Data Files (*.fits *.fit *.fts *.lc)")
-    #     files = []
-    #     if file_dialog.exec():
-    #         files = file_dialog.selectedFiles()
-    #     return files
+        outer_layout = QVBoxLayout()
+        self.setLayout(outer_layout)
+        file_table = QTableWidget()
+        file_table.setColumnCount(4)
+        file_table.setHorizontalHeaderLabels(['File', 'Telescope', 'Symbol', 'Remove'])
+        outer_layout.addWidget(file_table)
+
+        def file_dialog():
+            dialog = QFileDialog().getOpenFileNames(self, 'Select Files', '', 'Data Files (*.csv *.lc)')
+            if dialog.exec_():
+                filenames = dialog.selectedFiles()
+                for filename in filenames:
+                    self.file_list += filename
+            print(self.file_list)
+
+        select_files_button = QPushButton("Select Files")
+        select_files_button.clicked.connect(file_dialog)
+        outer_layout.addWidget(select_files_button)
+
+        def build_rows(file_paths):
+            for fp in file_paths:
+                row_position = file_table.rowCount()
+                file_table.insertRow(row_position)
+                telescope_line_edit = QLineEdit()
+                symbol_options = ['Point', 'Circle', 'Square', 'Star', 'Plus']
+                symbol_combobox = QComboBox()
+                symbol_combobox.addItems(symbol_options)
+                remove_button = QPushButton("Remove")
+                if fp in self.file_list:
+                    symbol_combobox.setCurrentText(str(self.file_list[fp["Symbol"]]))
+
+                file_table.setItem(row_position, 0, QTableWidgetItem(str(fp)))
+                file_table.setItem(row_position, 1, QTableWidgetItem(telescope_line_edit))
+                file_table.setItem(row_position, 2, QTableWidgetItem(symbol_combobox))
+                file_table.setItem(row_position, 3, QTableWidgetItem(remove_button))
+
+        def save_info():
+            num_rows = file_table.rowCount()
+            for row in num_rows:
+                file_name = str(file_table.item(row, 0))
+                telescope_text = str(file_table.item(row, 1).text()) if file_table.item(row, 1) is QLineEdit \
+                    else ""
+                symbol_choice = str(file_table.item(row, 1).currentText()) if file_table.item(row, 2) is QComboBox \
+                    else ""
+                file_dict = {"Telescope": telescope_text, "Symbol": symbol_choice}
+                self.file_list[file_name] = file_dict
+
+        def build_table():
+            pass
+
+        def delete_row(row):
+            file_name = str(file_table.item(row, 0))
+            del self.file_list[file_name]
+            build_table()
 
 
 class Window(QWidget):
