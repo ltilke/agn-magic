ec1b273ce77b9bdb024c08015870eabda8baa967
ltilke <lana.tilke@icloud.com>
2021-02-06 16:55:52 -0500

    added gridline option, improved formatting

--- graph.py
+++ graph.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-# from astropy.table import Table
 import os
 import json
 import pandas as pd
@@ -50,6 +49,7 @@ class GraphDataframe:
             color = "Gray"
             self.size = 5
             self.alpha = 0.5
+
         marker = config["files"][self.file][0]
 
         self.color = colors[color]
@@ -58,8 +58,10 @@ class GraphDataframe:
 
 def make_dataframes():
     dataframes = []
+
     for wavelength in config["wavelengths"].keys():
         for file in config["files"].keys():
+
             if os.path.splitext(file)[1] == ".csv":
                 cols = [
                     "Timestamp (JD)",
@@ -67,6 +69,7 @@ def make_dataframes():
                     config["source"] + " : Magnitude (Centroid)",
                     config["source"] + " : Error"
                 ]
+
                 df = pd.read_csv(file, skipinitialspace=True, usecols=cols)
                 df.insert(1, "Timestamp (MJD)", df["Timestamp (JD)"] - 2400000.5)
 
@@ -74,20 +77,13 @@ def make_dataframes():
                 if not clean_df.empty:
                     dataframe = GraphDataframe(clean_df, wavelength, file)
                     dataframes.append(dataframe)
+
             elif os.path.splitext(file)[1] == ".lc":
                 if "G" in config["wavelengths"]:
                     print(".lc files are not yet supported.")
-                    # # @author: Nikolas Korzoun
-                    # data = Table.read(file)
-                    # date = data['START']
-                    # date = (date / 60 / 60 / 24) + 2451910.5 - 2450000
-                    # flux = data['FLUX_100_300000'] * 100000
-                    # if config["error bars"]:
-                    #     error = data['ERROR_100_300000'] * 100000
-                    # else:
-                    #     error = 0
                 else:
                     del(config["files"][file])
+
             else:
                 print("Unknown file type (" + os.path.splitext(file)[1] + ")")
 
@@ -111,22 +107,28 @@ def get_legend_location():
 def make_graph():
     dataframes = make_dataframes()
     wavelengths = []
+
     for df in dataframes:
         wavelengths.append(df.wavelength) if df.wavelength not in wavelengths else wavelengths
 
-    fig, axs = plt.subplots(len(wavelengths), sharex=True)
+    # noinspection PyTypeChecker
+    fig, axs = plt.subplots(len(wavelengths), sharex=True, sharey=True)
     fig.suptitle(config["source"])
 
     for wavelength in wavelengths:
         ax_num = wavelengths.index(wavelength)
+
         for df in dataframes:
+            axs[ax_num].grid(color="tab:gray", linestyle="--", linewidth=0.25)
+
             if df.wavelength == wavelength:
                 axs[ax_num].scatter(x=df.dataframe["Timestamp (MJD)"],
                                     y=df.dataframe[config["source"] + " : Magnitude (Centroid)"],
                                     s=df.size,
                                     color=df.color, marker=df.marker, alpha=df.alpha,
-                                    label=config["files"][df.file][1] + " " + df.wavelength,
+                                    label=config["files"][df.file][1],
                                     )
+
                 if config["error bars"] is True:
                     axs[ax_num].errorbar(x=df.dataframe["Timestamp (MJD)"],
                                          y=df.dataframe[config["source"] + " : Magnitude (Centroid)"],
@@ -134,12 +136,14 @@ def make_graph():
                                          color=df.color, alpha=df.alpha,
                                          linestyle="None"
                                          )
+
                 axs[ax_num].set_xlabel("Timestamp (MJD)")
-                axs[ax_num].set_ylabel("Magnitude (Centroid)")
+                axs[ax_num].set_ylabel(df.wavelength + " Magnitude")
                 axs[ax_num].legend(loc=get_legend_location())
                 axs[ax_num].invert_yaxis()
                 axs[ax_num].figure.show()
 
+    plt.subplots_adjust(hspace=0)
     plt.show()
 
 
